8f3fb757e704fccd4ed8346d6a556a3ce243f967
Kartik Soneji <kartiksoneji@rocketmail.com>
2025-03-04 17:53:22 +0530

    Handle `AuthenticateUserRequest` commands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,78 @@
+"use client";
+
+import { useCallback } from "react";
 import Image from "next/image";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
 import styles from "./page.module.css";
+import { ArcanePlayer as ArcanePlayerType, getArcane } from "@/arcane";
 import { ArcanePlayer } from "@/components/ArcanePlayer";
 
+interface AuthInfo {
+  publicKey: string;
+  message: string;
+  signature: string;
+}
+
 export default function Home() {
+  const { publicKey, signMessage } = useWallet();
+
+  const getAuthInfo = useCallback(async (): Promise<AuthInfo | null> => {
+    if (typeof window === "undefined" || !publicKey || !signMessage)
+      return null;
+
+    const auth = JSON.parse(window.localStorage.auth ?? "{}");
+
+    const key = publicKey.toBase58();
+
+    if (!(key in auth)) {
+      const message = "Login to Game";
+      const signature = await signMessage(Buffer.from(message));
+      auth[key] = {
+        message,
+        signature: Buffer.from(signature).toString("base64"),
+      };
+      window.localStorage.auth = JSON.stringify(auth);
+    }
+
+    return {
+      publicKey: key,
+      ...auth[key],
+    };
+  }, [publicKey, signMessage]);
+
+  async function handleAuthenticateUserRequest() {
+    console.debug("handing cmd.AuthenticateUserRequest");
+
+    const auth = await getAuthInfo();
+    console.log(auth);
+
+    const arcane = getArcane();
+    arcane.emitUIEvent({
+      event: "AuthenticateUserResponse",
+      data: auth,
+    });
+  }
+
+  function setupArcaneEvents(arcane: ArcanePlayerType) {
+    arcane.onReceiveEvent(
+      "cmd.AuthenticateUserRequest",
+      handleAuthenticateUserRequest,
+    );
+  }
+
   return (
-    <ArcanePlayer
-      projectId={5104}
-      projectKey="4cd8a2f9-4688-48ed-af87-8554e451209f"
-      token="12mm9KpgD03P"
-    />
+    <>
+      <div style={{ textAlign: "right" }}>
+        <WalletMultiButton />
+      </div>
+
+      <ArcanePlayer
+        projectId={5415}
+        projectKey="b9805e31-8d58-46b8-bcdd-c7c6e7b70fcc"
+        token="JFFq6fKzekGi"
+        onPlayerLoaded={setupArcaneEvents}
+      />
+    </>
   );
 }
--- src/components/ArcanePlayer.tsx
+++ src/components/ArcanePlayer.tsx
@@ -1,102 +1,35 @@
 "use client";
 
 import Script from "next/script";
-import { useEffect } from "react";
-import { getArcane, initArcanePlayer } from "@/arcane";
+import { useCallback, useEffect } from "react";
+import {
+  ArcanePlayer as ArcanePlayerType,
+  getArcane,
+  initArcanePlayer,
+} from "@/arcane";
 
 export function ArcanePlayer({
   projectId,
   projectKey,
   token,
+  onPlayerLoaded,
 }: {
   projectId: number;
   projectKey: string;
   token: string;
+  onPlayerLoaded?: (arcane: ArcanePlayerType) => void;
 }) {
+  const handlePlayerLoaded = useCallback(() => {
+    console.log("ArcanePlayerLoaded");
+    const player = getArcane();
+    onPlayerLoaded?.(player);
+  }, [onPlayerLoaded]);
+
   useEffect(() => {
-    window.addEventListener("ArcanePlayerLoaded", () => {
-      console.log("ArcanePlayerLoaded");
-      const player = getArcane();
-
-      player.onReceiveEvent("CustomUIEventResponse", (response) => {
-        console.log({ ArcaneResponse: response });
-      });
-
-      player.onPlayerEvent("loading", () => {
-        console.log("loading");
-      });
-
-      player.onPlayerEvent("ready", () => {
-        console.log("ready");
-      });
-
-      player.onPlayerEvent("afkWarning", () => {
-        console.log("afkWarning");
-      });
-
-      player.onPlayerEvent("afkWarningDeactivate", () => {
-        console.log("afkWarningDeactivate");
-      });
-
-      player.onPlayerEvent("afkTimedOut", () => {
-        console.log("afkTimedOut");
-      });
-      // Emit event to Unreal Engine
-      player.emitUIEvent("MyCustomEventWithoutData");
-      player.emitUIEvent({
-        event: "MyCustomEventWithData",
-        data: {
-          foo: "bar",
-        },
-      });
-      // Receive event from Unreal Engine
-      player.onReceiveEvent("CustomUIEventResponse", (response) => {
-        console.log({ ArcaneResponse: response });
-      });
-      // If the response from UE is a json, you'll to set the descriptor
-      // of the listener as 'event.' + name or 'cmd.' + name, this response will
-      // look like this:
-      /*
-                {
-                    "event": "MyCustomEventWithData",
-                    "data": {
-                        "foo": "bar"
-                    }
-                }
-            */
-      player.onReceiveEvent("event.MyCustomEventWithData", (response) => {
-        console.log({ ArcaneResponse: response });
-      });
-      // For starting the stream programatically call:
-      player.play();
-
-      // For entering and exit fullscreen mode, this needs to be called
-      // after the user has any interaction with the site (click/touch or via button)
-      // or it will fail
-      // returns boolean for the current state of fullscreen element
-      player.toggleFullscreen();
-
-      // Receive file events, only override them if you don't want to use the default button/progress
-      player.onPlayerEvent("fileProgress", (progress: number) => {
-        console.log("File download progress:", progress);
-      });
-
-      player.onPlayerEvent(
-        "fileReceived",
-        (data: { file: Blob; extension: string }) => {
-          // Do what you need with the blob, for example, create a hidden anchor tag
-          // and download automatically
-          const a = document.createElement("a");
-          a.setAttribute("href", URL.createObjectURL(data.file));
-          a.style.display = "none";
-          a.setAttribute("download", `received_file.${data.extension}`);
-          document.body.append(a);
-          a.click();
-          a.remove();
-        },
-      );
-    });
-  }, []);
+    window.addEventListener("ArcanePlayerLoaded", handlePlayerLoaded);
+    return () =>
+      window.removeEventListener("ArcanePlayerLoaded", handlePlayerLoaded);
+  }, [handlePlayerLoaded]);
 
   return projectKey && projectId ? (
     <>
@@ -120,20 +53,6 @@ export function ArcanePlayer({
           data-token={token}
         ></div>
       </main>
-      <button
-        onClick={() => {
-          getArcane().emitUIEvent({
-            event: "AuthenticateUserResponse",
-            data: {
-              publicKey: "3FVrkePokrBYhQKthGjpTr8MCsPAymTStHuPCsLZ6SLX",
-              message: "Login to Game",
-              signature: "...",
-            },
-          });
-        }}
-      >
-        fire event
-      </button>
     </>
   ) : null;
 }
